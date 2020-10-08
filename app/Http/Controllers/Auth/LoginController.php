41037449fd12697cd68958263f09bbc2141b8412
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2020-10-08 16:45:49 -0500

    login settings

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,15 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated($user)
+    {
+        return response()->json($user,200);
+    }
 }
