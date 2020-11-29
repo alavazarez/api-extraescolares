22c20d8847af318444d5aab94c6d74cb1cec8f3f
Manuel de Jesus Alavazarez Hernández <eemanuel_11@hotmail.com>
2020-11-28 21:14:51 -0600

    Table-Model(Event-TypeEvent)
    Se crearon las migraciones y los modelos de Event y TypeEvent

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+        'name', 
+        'type', 
+        'date',
+        'time',
+        'Place',
+    ];
+
+    public function typeEvents()
+    {
+        return $this->belongsTo('App\Models\TypeEvent');
+    }
 }
--- database/migrations/2020_11_29_023455_create_type_events_table.php
+++ database/migrations/2020_11_29_023455_create_type_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEventsTable extends Migration
+class CreateTypeEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,10 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-        Schema::create('events', function (Blueprint $table) {
+        Schema::create('type_events', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('type');
-            $table->date('date');
-            $table->time('time');
-            $table->string('place');
             $table->timestamps();
-
-            $table->foreign('type')
-            ->references('name')
-            ->on('type_events');
         });
     }
 
@@ -35,6 +27,6 @@ class CreateEventsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('events');
+        Schema::dropIfExists('type_events');
     }
 }
