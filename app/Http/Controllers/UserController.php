1d536dd2fce8f5d685fa83cf3079f779e302e0a1
Manuel de Jesus Alavazarez Hernández <eemanuel_11@hotmail.com>
2021-01-14 22:00:56 -0600

    SendEmail
    Ya se pueden enviar emails

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\RegisterUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
     public function sendEmail($email)
     {
+        $route = 'http://localhost:8080/User/AddUser';
+        Mail::to($email)->send(new RegisterUser($route));
         return response()->json($email,200);
     }
 }
