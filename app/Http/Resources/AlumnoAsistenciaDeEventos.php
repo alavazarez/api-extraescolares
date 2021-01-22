6e1c8c5b9c209eb0a1d6f8716e03aab36ca8eb14
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2021-01-21 20:52:30 -0600

    tipo de datos

--- app/Http/Resources/AlumnoAsistenciaDeEventos.php
+++ app/Http/Resources/AlumnoAsistenciaDeEventos.php
@@ -2,48 +2,34 @@
 
 namespace App\Http\Resources;
 
-
 class AlumnoAsistenciaDeEventos 
 {
 
     private $resource;
 
-    public function __construct( $alumno, $asistencia = null, $isCompleted = false){
-       // $this->setEventos($event);
+    public function __construct( $alumno, $asistencia = null)
+    {
         $this->resource = (Object)[
             "alumno" => (Object)[],
-            "asistencias" => (Object)[],
+            "asistencias" => [],
         ];
         $this->setAlumno($alumno);
         $this->setAsistencia($asistencia);
     }
 
-    private function setAlumno($alumno){
-        //dd($alumno->nombre);
+    private function setAlumno( Object $alumno)
+    {
         $this->resource->alumno = $alumno;
     }
 
-    private function setEventos($eventos){
-        $item = (Object) [];
-        foreach($eventos as $event){
-            $item->id = $event->id;
-            $item->type = $event->type;
-            $item->date = $event->date;
-            $this->resource->events[] = $item;
-        }
-    }
-
-    private function setAsistencia($asistencia){
+    private function setAsistencia(Array $asistencia)
+    {
         $this->resource->asistencias = $asistencia;
     }
-
-    private function setIsCompleted($isCompleted){
-        
-    }
-
 
     public function toJson()
     {
         return json_encode($this->resource);
     }
+
 }
